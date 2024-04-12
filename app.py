db2483e26462d6cc73c63c04382cc6da845ee093
joffreynk <joyjoffrey@gmail.com>
2024-04-12 15:17:19 +0200

    finalize the application

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-  #! python3
+  # ! python3
   # randomQuizGenerator.py - Creates quizzes with questions and answers in
   # random order, along with the answer key.
-import random
+import random, os, pathlib as p
 # The quiz data. Keys are states and values are their capitals.
 allStates = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -22,26 +22,26 @@ allStates = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 # Generate 35 quiz files.
 for quizNum in range(35):
   # TODO: Create the quiz and answer key files.
-  file = open('./quizzes/quiz{}.txt'.format(quizNum+1), 'w')
+  file = open('Quiz{}.txt'.format(quizNum+1), 'w')
   # TODO: Write out the header for the quiz.
-  file.write('Full Name:_________________________________ \nClass:________ \nGeography quiz\n\nInstructions:\nPlease attempt all the questions.\nThe use of the computer or map is highly discouraged\n')
+  file.write('Full Name:_________________________________ \nClass:________ \nGeography quiz\n\nInstructions:\nPlease attempt all the questions.\nThe use of the computer or map is highly discouraged\n\n\n')
   # TODO: Shuffle the order of the states.
-  states = allStates.keys()
+  states = list(allStates.keys())
   random.shuffle(states)
   random.shuffle(states)
   s = states[1:12]
   
   # TODO: Loop through all 50 states, making a question for each.
   for q in s:
-    capitals = allStates.values()
-    del capitals[q]
+    capitals = list(allStates.values())
+    capitals.remove(allStates[q])
     random.shuffle(capitals)
     random.shuffle(capitals)
     c = capitals[15:18]
     c.append(allStates[q])
     random.shuffle(c)
     file.write('what is the capital city of the {}? \n'.format(q))
-    file.write('A. {}\t B. {}\t C. {}\t D. {}').format(c[1], c[0], c[3], c[2])
+    file.write('A. {}\t B. {}\t C. {}\t D. {}\n\n'.format(c[1], c[0], c[3], c[2]))
   
   file.close()
 
