9d46812da1ad307fc58629d951505e5ecbaf864a
joffreynk <joyjoffrey@gmail.com>
2024-04-12 14:53:01 +0200

    finalize quiz implementation

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-   #! python3
-   # randomQuizGenerator.py - Creates quizzes with questions and answers in
-   # random order, along with the answer key.
+  #! python3
+  # randomQuizGenerator.py - Creates quizzes with questions and answers in
+  # random order, along with the answer key.
 import random
-   # The quiz data. Keys are states and values are their capitals.
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+# The quiz data. Keys are states and values are their capitals.
+allStates = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
   'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
   'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':'Springfield',
@@ -19,12 +19,29 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
   'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':'Montpelier',
   'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
-   # Generate 35 quiz files.
+# Generate 35 quiz files.
 for quizNum in range(35):
-    # TODO: Create the quiz and answer key files.
+  # TODO: Create the quiz and answer key files.
+  file = open('./quizzes/quiz{}.txt'.format(quizNum+1), 'w')
+  # TODO: Write out the header for the quiz.
+  file.write('Full Name:_________________________________ \nClass:________ \nGeography quiz\n\nInstructions:\nPlease attempt all the questions.\nThe use of the computer or map is highly discouraged\n')
+  # TODO: Shuffle the order of the states.
+  states = allStates.keys()
+  random.shuffle(states)
+  random.shuffle(states)
+  s = states[1:12]
+  
+  # TODO: Loop through all 50 states, making a question for each.
+  for q in s:
+    capitals = allStates.values()
+    del capitals[q]
+    random.shuffle(capitals)
+    random.shuffle(capitals)
+    c = capitals[15:18]
+    c.append(allStates[q])
+    random.shuffle(c)
+    file.write('what is the capital city of the {}? \n'.format(q))
+    file.write('A. {}\t B. {}\t C. {}\t D. {}').format(c[1], c[0], c[3], c[2])
+  
+  file.close()
 
-    # TODO: Write out the header for the quiz.
-
-    # TODO: Shuffle the order of the states.
-
-    # TODO: Loop through all 50 states, making a question for each.
